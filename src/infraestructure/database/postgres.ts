a5e1ad439979f1ad9f8d29e732e8c00226f37edd
unknown <martinsennaoficial@gmail.com>
2025-05-30 13:14:18 -0300

    initial commit 5

--- src/infraestructure/database/postgres.ts
+++ src/infraestructure/database/postgres.ts
@@ -4,6 +4,7 @@ import { Adress, Country, Locality, Province } from "../models/locations.models"
 import * as dotenv from "dotenv";
 
 dotenv.config();
+console.log(process.env.DATABASE_URL)
 export const AppDataSource = new DataSource({
     type: "postgres",
     url: process.env.DATABASE_URL,

33d45ed3152a6e4e65e0a2b8f233d742fd373ea4
unknown <martinsennaoficial@gmail.com>
2025-05-30 12:47:18 -0300

    initial commit 2

--- src/infraestructure/database/postgres.ts
+++ src/infraestructure/database/postgres.ts
@@ -5,6 +5,7 @@ import { Adress, Country, Locality, Province } from "../models/locations.models"
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
+    url: process.env.DATABASE_URL,
     port: 5432,
     username: "postgres",
     password: "admin",

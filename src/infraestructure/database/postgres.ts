c10f481f351f434f209e42290a0c251788d1eafc
unknown <martinsennaoficial@gmail.com>
2025-05-30 13:09:10 -0300

    initial commit 4

--- src/infraestructure/database/postgres.ts
+++ src/infraestructure/database/postgres.ts
@@ -1,15 +1,12 @@
 import { DataSource } from "typeorm";
 import { UserProfile } from "../models/userProfile.model"
 import { Adress, Country, Locality, Province } from "../models/locations.models";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
     url: process.env.DATABASE_URL,
-    port: 5432,
-    username: "postgres",
-    password: "admin",
-    database: "tesis",
     synchronize: true,
     logging: true,
     entities: [Locality, Province, Country, Adress, UserProfile],
